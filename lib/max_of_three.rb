3332f14e800997e14d0e26f889476714883e2a1f
Filip Torphage <filip.torphage@itggot.se>
2018-04-18 14:01:14 +0200

    added max_of_three

---
+++ lib/max_of_three.rb
@@ -0,0 +1,9 @@
+def max_of_three(num1, num2, num3)
+    if num1 >= num2 && num1 >= num3            
+        return num1
+    elsif num2 >= num3
+        return num2
+    else
+        return num3
+    end
+end
