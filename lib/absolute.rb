4452b77bf43cbe71fd16077036bf2bfbf5afdc44
Filip Torphage <filip.torphage@itggot.se>
2018-04-25 14:02:19 +0200

    added doc

--- lib/absolute.rb
+++ lib/absolute.rb
@@ -1,3 +1,20 @@
+# num - The Integer that is wanted as an absolute
+#
+# Examples
+#
+#   absolute(5)
+#   #=> 5
+#
+#   absolute(100)
+#   #=> 100
+#
+#   absolute(-200)
+#   #=> 200
+#
+#   absolute(-10)
+#   #=> 10
+#
+# Returns the absolute value of the Integer num
 def absolute(num)
     if num < 0
         return num * -1
